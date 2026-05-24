8102f830674821fa965a39d1bb54c719ed9f718b
Kuba-Kowal <148563319+Kuba-Kowal@users.noreply.github.com>
2026-05-24 01:12:44 +0100

    Commit Re-Query Update
    New domains are now added to a queue system. That the program reoccurs through and performs a new search for.
    
    Next session I want to fix the logic to be more clean, with a more organised operational pipeline.
    
    Fetch → Normalize → Store → Clean / Heuristics → Output

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 import json
 import time
 
-DOMAIN_NAME = "example.com"
-RATE_LIMIT = 0.5
 
+
+# Original request logs
 class RawCTLog:
     def __init__(self, asn, date, issuer):
         self.asn = asn
@@ -14,6 +14,7 @@ class RawCTLog:
     def __repr__(self):
         return f"Domain(asn={self.asn}, date={self.date}, issuer={self.issuer})"
 
+# Cleaned up logs
 class CTLog():
     def __init__(self, asn, issuer, first_date, last_date):
         self.asn = asn
@@ -22,10 +23,11 @@ class CTLog():
         self.issuer = issuer
     
     def __repr__(self):
-        return f"""{[asn for asn in self.asn]}
+        return (f"""{[asn for asn in self.asn]}
 issuer={self.issuer.strip('"')} 
-[{self.first_date},{self.last_date}]
-"""
+[{self.first_date},{self.last_date}]""")
+
+
 
 # Extract issuer name
 def clean_issuer_name(issuer):
@@ -33,6 +35,8 @@ def clean_issuer_name(issuer):
         i = i.strip()
         if i.startswith('O='):
             return i.replace("O=", "")
+        else:
+            return "Unkown"
 
 # Remove time from DateTime
 def clean_date(date):
@@ -40,9 +44,11 @@ def clean_date(date):
     return date[0]
 
 # Remove duplicate logs and sort by date added to repository descending
-def deduplicate_logs(logs: list):
+def dedup_request(logs: list):
     groups = {}
+    new_queries = []
 
+    # Create a dictionary of asn, issuer -> timestamp mappings
     for log in logs:
         key = (tuple(log.asn), log.issuer)
 
@@ -53,42 +59,114 @@ def deduplicate_logs(logs: list):
 
     cleaned_logs = []
 
+    # For every combination of asn <-> issuer. Any domains that are new, add them to the queue list. 
     for (asn, issuer), dates in groups.items():
+        for domain in asn:
+            domain = domain.strip(",() ")
+            if domain in new_queries:
+                continue
+            new_queries.append(domain)
+
+        # Create new object for duplicate ASNs including latest log and oldest log date.
         cleaned_logs.append(
             CTLog(asn, issuer, max(dates), min(dates))
         )
 
     cleaned_logs.sort(key=lambda x: x.first_date, reverse=True)
-    return cleaned_logs
 
-request_success = False
+    return cleaned_logs, new_queries
 
 # Make crt.sh request in json format
-while request_success != True:
-    req = requests.get(f"https://crt.sh/?q={DOMAIN_NAME}&output=json")
+def make_crt_req(DOMAIN_NAME, RATE_LIMIT, MAX_RETRIES):
+    session = requests.Session()
 
-    if req.status_code == 200:
-        request_success = True
+    session.headers.update({"User-Agent": "Mozilla/5.0"})
 
-    time.sleep(RATE_LIMIT)
+    url = f"https://crt.sh/?q={DOMAIN_NAME}&output=json"
 
-raw_ct_log_list: list[object] = []
+    print(f"[+] CRT.SH QUERY -> {DOMAIN_NAME}")
 
-# Convert crt.sh data into objects
-for item in req.json():
-    if not item.get("entry_timestamp"):
-        continue
+    # Attempt connection to server
+    for attempt in range(1, MAX_RETRIES + 1):
+        try:
+            response = session.get(url, timeout=60)
 
-    domain = RawCTLog(
-        item['name_value'].split(),
-        clean_date(item['entry_timestamp']),
-        clean_issuer_name(item['issuer_name'])
-        )
+            if response.status_code == 200:
+                print("[+] SUCCESS")
+                break
 
-    raw_ct_log_list.append(domain)
+            else:
+                pass
 
-clean_logs = deduplicate_logs(raw_ct_log_list)
+        except requests.exceptions.RequestException as e:
+            print(f"[-] Request failed: {e}")
+
+        time.sleep(RATE_LIMIT)
+
+    else:
+        print("[-] Failed after max retries")
+
+        return None
+
+
+    # Convert crt.sh data into objects
+    raw_ct_log_list = []
+    for item in response.json():
+        if not item.get("entry_timestamp"):
+            continue
+
+        domain = RawCTLog(
+            item['name_value'].split(),
+            clean_date(item['entry_timestamp']),
+            clean_issuer_name(item['issuer_name'])
+            )
+
+        raw_ct_log_list.append(domain)
+
+    return raw_ct_log_list
+
+def __main__(DOMAIN_NAME):
+    RATE_LIMIT = 0.5
+    MAX_RETRIES = 20
+
+    queue = set()
+    searched_queries = set()
+    output = []
+
+    queue.add(DOMAIN_NAME)
+    while len(queue) != 0:
+        # Take object out of queue
+        current_query = queue.pop()
+        searched_queries.add(current_query)
+
+        # Make request with latest item removed from queue
+        request_output = make_crt_req(current_query, RATE_LIMIT, MAX_RETRIES)
+        if request_output == None:
+            continue
+        
+        # Dedupliacte, organise and append new queries 
+        clean_logs, new_queries = dedup_request(request_output)
+
+        for log in clean_logs:
+            output.append(log)
+
+        # Add new queries to queue
+        if len(new_queries) > 0:
+            for query in new_queries:
+                if query in searched_queries:
+                    continue
+                queue.add(query)
+
+    print("\n[+] Queue Finished")
+    time.sleep(3)
+
+    return output
+
+DOMAIN_NAME = "example.com"
+output = __main__(DOMAIN_NAME)
 
 # Print logs
-for log in clean_logs:
-    print(log)
+for result in output:
+    print(result)
+    print(type(result.asn))
+
