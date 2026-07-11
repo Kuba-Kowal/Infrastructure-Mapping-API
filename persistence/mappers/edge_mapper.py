d8e2a0a29f3538ff553e741992e4ac2d27753dad
Jakub <148563319+Kuba-Kowal@users.noreply.github.com>
2026-07-11 02:06:32 +0100

    Create edge_mapper.py

---
+++ persistence/mappers/edge_mapper.py
@@ -0,0 +1,14 @@
+from core.graph import Graph
+from core.models import *
+from datetime import datetime
+
+def map_edges(graph: Graph) -> tuple[str, str, str]:
+    edge_mappings = ["cert_to_fqdn", "fqdn_to_dns", "fqdn_to_pdns", "ip_to_prefix", "prefix_to_asn", "asn_to_org"]
+
+    edges = []
+
+    for bucket in edge_mappings:
+        for obj in graph.__getattribute__(bucket):
+            edges.append((obj.source_data, obj.target_data, obj.observed_at))
+
+    return edges
