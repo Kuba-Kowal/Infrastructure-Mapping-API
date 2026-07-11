59cad3638c7dd5bffe43219389559a2d8b71ab61
Jakub <148563319+Kuba-Kowal@users.noreply.github.com>
2026-07-11 02:08:40 +0100

    Update graph.py

--- core/graph.py
+++ core/graph.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass, field, asdict
 from core.models import *
 from core.generate_hash import generate_hash
+import tldextract
 
 @dataclass(slots=True)
 class Graph:
+    apex_domain: str = field(default_factory=str)
+
     certificates: dict[str, Certificate] = field(default_factory=dict)
     fqdns: dict[str, FQDN] = field(default_factory=dict)
     ips: dict[str, IPAddress] = field(default_factory=dict)
@@ -29,21 +32,16 @@ class Graph:
         "DNSRecord": "dns_records"
     }
 
-    edge_bucket_table = {
-        "CerttoFQDN": "cert_to_fqdn",
-        "FQDNtoDNS": "fqdn_to_dns",
-        "FQDNtoPassiveDNS": "fqdn_to_pdns",
-        "IPtoPrefix": "ip_to_prefix",
-        "PrefixtoASN": "prefix_to_asn",
-        "AStoOrganisation": "asn_to_org",
-        "FQDNtoDNS": "fqdn_to_dns"
-    }
+    def extract_apex(self, domain) -> str:
+        result = tldextract.extract(domain)
+
+        return f"{result.domain}.{result.suffix}"
 
     def get_domains(self) -> list[str]:
-        return [fqdn.domain for fqdn in self.fqdns.values()]
+        return [fqdn.data for fqdn in self.fqdns.values()]
 
     def get_ips(self) -> list[str]:
-        return [ipaddress.ip for ipaddress in self.ips.values()]
+        return [ipaddress.data for ipaddress in self.ips.values()]
 
     def get_object_type(self, obj: Object) -> type(obj):
         try:
@@ -62,28 +60,15 @@ class Graph:
         else:
             self.__getattribute__(node_bucket)[node_hash] = obj
 
-    def add_edge(self, obj, last_observed="") -> None:
-        src_object_type = self.get_object_type(obj.__slots__[0])
-        target_object_type = self.get_object_type(obj.__slots__[1]) 
-
-        edge_bucket = self.edge_bucket_table[obj.__class__.__name__]
-
-        self.__getattribute__(edge_bucket).add(obj)
-
-
-    def to_dict(self) -> [dict[str, [dict[str, str]]]]:
+    def json_to_dict(self) -> [dict[str, [dict[str, str]]]]:
         return {
-            "certificates": {
-                cert_hash: {attr_name: value for attr_name, value in asdict(cert_values).items()} 
-                for cert_hash, cert_values in self.certificates.items()
-                },
-
-            "fqdns": {fqdn_hash: fqdn.domain for fqdn_hash, fqdn in self.fqdns.items()},
-            "ips": {ip_hash: ip_address.ip for ip_hash, ip_address in self.ips.items()},
-            "prefixes": {prefix_hash: prefix.prefix for prefix_hash, prefix in self.prefixes.items()},
-            "asns": {asn_hash: asn.as_number for asn_hash, asn in self.asns.items()},
-            "organisations": {org_hash: organisation.organisation for org_hash, organisation in self.organisations.items()},
-            "dns_records": {dns_hash: dns_record.data for dns_hash, dns_record in self.dns_records.items()},
+            "certificates": {cert_hash: {"id": cert.data, "properties": cert.properties} for cert_hash, cert in self.certificates.items()},
+            "fqdns": {fqdn_hash: fqdn.data for fqdn_hash, fqdn in self.fqdns.items()},
+            "ips": {ip_hash: ip_address.data for ip_hash, ip_address in self.ips.items()},
+            "prefixes": {prefix_hash: prefix.data for prefix_hash, prefix in self.prefixes.items()},
+            "asns": {asn_hash: asn.data for asn_hash, asn in self.asns.items()},
+            "organisations": {org_hash: organisation.data for org_hash, organisation in self.organisations.items()},
+            "dns_records": {dns_hash: {"data": dns_record.data, "rtype": dns_record.properties} for dns_hash, dns_record in self.dns_records.items()},
 
             "cert_to_fqdn": [asdict(r) for r in self.cert_to_fqdn],
             "fqdn_to_dns": [asdict(r) for r in self.fqdn_to_dns],
